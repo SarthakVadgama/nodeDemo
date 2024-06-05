cbd0357676dd610534257698f5f3d53af0a34a4d
Sarthak <43034995+SarthakVadgama@users.noreply.github.com>
2024-06-05 11:22:44 +0530

    demo env

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const cors = require("cors");
 const notFound = require("./middlewares/notFound");
 const productRoutes = require("./routes/product");
+const mongoose = require("mongoose");
 require("dotenv").config();
+const a = 53;
+const b = 25;
 
 app.use(express.json());
 app.use(cors());
@@ -12,6 +15,14 @@ app.use("/api/v1/product", productRoutes);
 
 app.use(notFound);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port http://localhost:${process.env.PORT}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.CONSTR);
+    app.listen(process.env.PORT);
+    const sum = a + b;
+    console.log(`Listening on port http://localhost:${process.env.PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
